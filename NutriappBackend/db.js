fc02a93d21b2820d511705b66aefdb2339576220
Nemit027 <benjamin.huechuqueo@mayor.cl>
2025-11-16 17:31:58 -0300

    Actualizar db.js para que funcione con Render y SSL

--- NutriappBackend/db.js
+++ NutriappBackend/db.js
@@ -1,16 +1,22 @@
-require('dotenv').config(); // Carga las variables del archivo .env
-const { Pool } = require('pg'); // Importa el "traductor" de PostgreSQL
+require('dotenv').config(); 
+const { Pool } = require('pg');
 
-// Crea la conexión
-const pool = new Pool({
+// Esta configuración es inteligente:
+// 1. Le dice a 'pg' que use la URL completa (DATABASE_URL) si existe (así lo usará Render).
+// 2. Si no existe, usará las variables separadas de tu .env (así lo usas tú en tu PC).
+const config = {
+  connectionString: process.env.DATABASE_URL,
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+  // Activa SSL SÓLO si estamos en producción (cuando DATABASE_URL exista)
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+};
 
-// Exporta una función para que otros archivos puedan hacer consultas
+const pool = new Pool(config);
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
