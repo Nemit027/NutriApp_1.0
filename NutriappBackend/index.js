4664dbbefe92f18cbf684f73ee68bc2e2855ce11
Nemit027 <benjamin.huechuqueo@mayor.cl>
2025-11-16 21:16:37 -0300

    Añadir host 0.0.0.0 para Render

--- NutriappBackend/index.js
+++ NutriappBackend/index.js
@@ -634,6 +634,6 @@ app.delete('/api/community/posts/:postId', checkAuth, async (req, res) => {
 
 // 5. Iniciar el servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
